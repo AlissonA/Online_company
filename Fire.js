ddc565265535bf7115ffcfb316ac634ce6e9365b
unknown <alisson.alves213@gmail.com>
2019-11-19 09:18:33 -0300

    Primeira entrega.

---
+++ Fire.js
@@ -0,0 +1,88 @@
+import firebase from 'firebase'; 
+
+class Fire {
+  constructor() {
+    this.init();
+    this.observeAuth();
+  }
+
+  init = () => {
+    if (!firebase.apps.length) {
+      firebase.initializeApp({
+        apiKey: "AIzaSyBbG_8VRVuAQcEHW6avYz5cO27pJ1wI_sE",
+        authDomain: "online-company.firebaseapp.com",
+        databaseURL: "https://online-company.firebaseio.com",
+        projectId: "online-company",
+        storageBucket: "online-company.appspot.com",
+        messagingSenderId: "944553625909",
+        appId: "1:944553625909:web:90afc33953153ea1d7647c",
+        measurementId: "G-KDXKD7XBPC",
+      });
+    }
+  };
+
+  observeAuth = () =>
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+
+  onAuthStateChanged = user => {
+    if (!user) {
+      try {
+        firebase.auth().signInAnonymously();
+      } catch ({ message }) {
+        alert(message);
+      }
+    }
+  };
+
+  get uid() {
+    return (firebase.auth().currentUser || {}).uid;
+  }
+
+  get ref() {
+    return firebase.database().ref('messages');
+  }
+
+  parse = snapshot => {
+    const { timestamp: numberStamp, text, user } = snapshot.val();
+    const { key: _id } = snapshot;
+    const timestamp = new Date(numberStamp);
+    const message = {
+      _id,
+      timestamp,
+      text,
+      user,
+    };
+    return message;
+  };
+
+  on = callback =>
+    this.ref
+      .limitToLast(20)
+      .on('child_added', snapshot => callback(this.parse(snapshot)));
+
+  get timestamp() {
+    return firebase.database.ServerValue.TIMESTAMP;
+  }
+  // send the message to the Backend
+  send = messages => {
+    for (let i = 0; i < messages.length; i++) {
+      const { text, user } = messages[i];
+      const message = {
+        text,
+        user,
+        timestamp: this.timestamp,
+      };
+      this.append(message);
+    }
+  };
+
+  append = message => this.ref.push(message);
+
+  // close the connection to the Backend
+  off() {
+    this.ref.off();
+  }
+}
+
+Fire.shared = new Fire();
+export default Fire;
---
+++ components/Main.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+class Main extends React.Component {
+  static navigationOptions = {
+    title: 'Online company',
+  };
+
+  state = {
+    name: '',
+  };
+
+  onPress = () =>
+    this.props.navigation.navigate('Chat', { name: this.state.name });
+
+  onChangeText = name => this.setState({ name });
+
+  render() {
+    return (
+      <View>
+        <Text style={styles.title}>Digite seu nome:</Text>
+        <TextInput
+          style={styles.nameInput}
+          placeHolder="Digite seu nome"
+          onChangeText={this.onChangeText}
+          value={this.state.name}
+        />
+        <TouchableOpacity onPress={this.onPress}>
+          <Text style={styles.buttonText}>Próxima</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const offset = 24;
+
+const styles = StyleSheet.create({
+  title: {
+    marginTop: offset,
+    marginLeft: offset,
+    fontSize: offset,
+  },
+  nameInput: {
+    height: offset * 2,
+
+    margin: offset,
+    paddingHorizontal: offset,
+    borderColor: '#111111',
+    borderWidth: 1,
+  },
+  buttonText: {
+    marginLeft: offset,
+    fontSize: offset,
+  },
+});
+
+export default Main;
+// https://snack.expo.io/@bacon/firebase-basic-chat-video-tutorial
